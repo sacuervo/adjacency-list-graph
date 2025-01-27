28f6b81301111d9d0ee0d62b25c55f0d2fed30e8
sacuervo <cuervo.santiago@gmail.com>
2025-01-27 10:35:27 -0500

    Add printGraph() at Graph

--- src/main/java/me/santiagocuervo/Graph.java
+++ src/main/java/me/santiagocuervo/Graph.java
@@ -16,4 +16,20 @@ public class Graph {
         first.neighbors.add(second);
         second.neighbors.add(first);
     }
+
+    public String printGraph() {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < nodeList.size(); i++) {
+            s.append(nodeList.get(i).name).append(": ");
+            for (int j = 0; j < nodeList.get(i).neighbors.size(); j++) {
+                if (j == nodeList.get(i).neighbors.size() - 1) {
+                    s.append(nodeList.get(i).neighbors.get(j).name);
+                } else {
+                    s.append((nodeList.get(i).neighbors.get(j).name) + " -> ");
+                }
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
 }

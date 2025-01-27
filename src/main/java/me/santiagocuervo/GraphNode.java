f73da9562e242bafe8bb23647c3cc669a8ec77d8
sacuervo <cuervo.santiago@gmail.com>
2025-01-27 09:42:39 -0500

    Add GraphNode implementation

---
+++ src/main/java/me/santiagocuervo/GraphNode.java
@@ -0,0 +1,15 @@
+package me.santiagocuervo;
+
+import java.util.ArrayList;
+
+public class GraphNode {
+    public String name;
+    public int index;
+
+    public ArrayList<GraphNode> neighbors = new ArrayList<>();
+
+    public GraphNode(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+}
